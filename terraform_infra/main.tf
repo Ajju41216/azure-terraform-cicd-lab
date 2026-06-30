c89e0126129b60d56020b42fff6fd87da48db5f7
Ajmer Singh <singhchahalajmer@gmail.com>
2026-06-30 23:27:52 +0530

    corrected the code

--- terraform_infra/main.tf
+++ terraform_infra/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet1" {
   name                 = "vm_subnet"
-  resource_group_name  = azurerm_resource_group.rg1.location
+  resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
